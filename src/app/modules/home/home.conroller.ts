561d583e58aaf01dfca36400ee27ea172ed85f6c
SMTamim <tamimmahmud0@gmail.com>
2025-04-30 19:17:57 +0600

    init

--- src/app/modules/home/home.conroller.ts
+++ src/app/modules/home/home.conroller.ts
@@ -1,9 +1,10 @@
+import config from '../../config';
 import catchAsync from '../../utils/catchAsync';
 
 const index = catchAsync(async (req, res) => {
   res.status(200).json({
     success: true,
-    message: 'Bike service API is running!',
+    message: `😎 SFF API is chilling on '${(config.NODE_ENV as string).toUpperCase()}' environment!`,
   });
 });
 
