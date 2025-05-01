ebc93779ad0bb3ce445286e818cd19ea5cbd6537
Th3At0nic <islammdrahatul@gmai.com>
2025-05-01 10:40:04 +0600

    modified the process of image upload to cloudinary

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -21,6 +21,6 @@ export default {
   cloudinary: {
     cloudName: process.env.CLOUDINARY_CLOUD_NAME,
     apiKey: process.env.CLOUDINARY_API_KEY,
-    secretKey: process.env.CLOUDINARY_API_SECRET,
+    apiSecret: process.env.CLOUDINARY_API_SECRET,
   },
 };
