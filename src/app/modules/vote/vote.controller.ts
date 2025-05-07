7ab3a1acfe8ad2e25a00711641d53314aa9f78f2
SMTamim <tamimmahmud0@gmail.com>
2025-05-07 23:06:04 +0600

    updated vote services

--- src/app/modules/vote/vote.controller.ts
+++ src/app/modules/vote/vote.controller.ts
@@ -6,7 +6,7 @@ import { VoteServices } from './vote.service';
 const createOne = catchAsync(async (req, res) => {
   const result = await VoteServices.createOneIntoDB(req.body, req.user);
   sendResponse(res, {
-    statusCode: httpStatus.OK,
+    statusCode: httpStatus.CREATED,
     success: true,
     message: 'Vote created successfully',
     data: result,
