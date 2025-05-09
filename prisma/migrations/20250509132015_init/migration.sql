18c9f75263515ba7253431c266448424d04c2dfc
alamshuvo <alamshuvozeny019@gmail.com>
2025-05-09 22:51:47 +0600

    search term added

--- prisma/migrations/20250509132015_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ComentActivationStatus" AS ENUM ('APPROVED', 'PENDING', 'REJECTED');
-
--- AlterTable
-ALTER TABLE "comments" ADD COLUMN     "isApproved" "ComentActivationStatus" NOT NULL DEFAULT 'PENDING';
